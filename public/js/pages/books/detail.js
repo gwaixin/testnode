1d641c2f1250a943a51f1445754a1133603ad331
Nichole John Martinez <xin_martinez@yahoo.com>
2016-04-20 17:46:11 +0800

    Fix books detail js

--- public/js/pages/books/detail.js
+++ public/js/pages/books/detail.js
@@ -3,14 +3,25 @@ $(document).ready(function() {
 	var idUser = $('#user-id').val();
 	var firstName = $('#user-name').val();
 	var ioRoom = $('#book-page').val();
+	var bookid = $('#book-id').val();
 
 	$('#send-comment').click(function() {
 		var message = $('#comment-text').val();
-		socket.emit('comment message', {message: message, userid: idUser, firstname:firstName, ioRoom:ioRoom});
+		socket.emit('comment message', {
+			message: message,
+			userid: idUser,
+			firstname: firstName,
+			ioRoom: ioRoom,
+			bookid: bookid
+		});
 		$('#comment-text').val('');
 	});
 
-	socket.on('comment message', function(commentRow) {
-		$('#comments').prepend(commentRow);
+	socket.on('comment message', function(data) {
+		if (data.result) { 
+			$('#comments').prepend(data.message);
+		} else {
+			errorMessage(data.message);
+		}
 	});
 });
